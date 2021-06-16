7688665bdb62aacce4e80e38a42fe0991a7b736e
Rizky Mulya Rachmat <70185920+rizmulmatrizky@users.noreply.github.com>
2021-06-16 16:22:27 +0700

    Add files via upload

---
+++ admin/dashboard.php
@@ -0,0 +1,108 @@
+<?php 
+$p = mysqli_query($conn,'SELECT count(*) as ruangan from ruangan');
+$q = mysqli_fetch_array($p);
+?>
+
+<?php 
+$r = mysqli_query($conn,'SELECT count(*) as kegiatan from kegiatan');
+$s = mysqli_fetch_array($r);
+?>
+
+<?php 
+$key = mysqli_query($conn,'SELECT count(*) as user from user');
+$b = mysqli_fetch_array($key);
+?>
+
+<?php 
+$t = mysqli_query($conn,'SELECT count(*) as pemakaian from pemakaian');
+$z = mysqli_fetch_array($t);
+?>
+
+<div class="main-panel">
+			<div class="content">
+				<div class="page-inner">
+					<div class="row">
+						<div class="col-sm-6 col-md-3">
+							<div class="card card-stats card-round">
+								<div class="card-body ">
+									<div class="row align-items-center">
+										<div class="col-icon">
+											<div class="icon-big text-center icon-primary bubble-shadow-small">
+												<i class="fas fa-check-circle"></i>
+											</div>
+										</div>
+										<div class="col col-stats ml-3 ml-sm-0">
+											<div class="numbers">
+												<p class="card-category">Data Ruangan</p>
+												<h4 class="card-title"><?php echo $q['ruangan'] ?></h4>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						<div class="col-sm-6 col-md-3">
+							<div class="card card-stats card-round">
+								<div class="card-body ">
+									<div class="row align-items-center">
+										<div class="col-icon">
+											<div class="icon-big text-center icon-primary bubble-shadow-small">
+												<i class="fas fa-check-circle"></i>
+											</div>
+										</div>
+										<div class="col col-stats ml-3 ml-sm-0">
+											<div class="numbers">
+												<p class="card-category">Data Kegiatan</p>
+												<h4 class="card-title"><?php echo $s['kegiatan'] ?></h4>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-sm-6 col-md-3">
+							<div class="card card-stats card-round">
+								<div class="card-body ">
+									<div class="row align-items-center">
+										<div class="col-icon">
+											<div class="icon-big text-center icon-primary bubble-shadow-small">
+												<i class="fas fa-chart-bar"></i>
+											</div>
+										</div>
+										<div class="col col-stats ml-3 ml-sm-0">
+											<div class="numbers">
+												<p class="card-category">Data User</p>
+												<h4 class="card-title"><?php echo $b['user'] ?></h4>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-sm-6 col-md-3">
+							<div class="card card-stats card-round">
+								<div class="card-body ">
+									<div class="row align-items-center">
+										<div class="col-icon">
+											<div class="icon-big text-center icon-primary bubble-shadow-small">
+												<i class="fas fa-newspaper"></i>
+											</div>
+										</div>
+										<div class="col col-stats ml-3 ml-sm-0">
+											<div class="numbers">
+												<p class="card-category">Data Peminjaman</p>
+												<h4 class="card-title"><?php echo $z['pemakaian'] ?></h4>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
